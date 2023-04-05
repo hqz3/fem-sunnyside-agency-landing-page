a897efb50ccd948a3abbde568350f64fa58d2474
hqz3 <heqzheng@gmail.com>
2023-04-05 16:59:39 -0400

    Merge "hamburger menu" and "menu" elements for DRY

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-const hamburgerEl = document.querySelector(".hamburger");
-const hamburgerMenuEl = document.querySelector(".hamburger-menu");
+const hamburgerEl = document.querySelector(".hamburger-icon");
+const menuEl = document.querySelector(".menu");
+const menuWidth = 350;
 
 hamburgerEl.addEventListener("click", (e) => {
   e.stopPropagation();
-  hamburgerMenuEl.classList.toggle("hide");
-  if (hamburgerMenuEl.classList.contains("hide")) return;
-
-  const { right, width } = hamburgerMenuEl.getBoundingClientRect();
-  const rightOffset = screen.width - right;
-
-  // If the screen width is less than the hamburger menu element,
-  // resize the hamburger menu element
-  // Otherwise, if the screen width is greater than the hamburger menu element,
-  // Reset the hamburger menu element to its default width
-  if (screen.width < width + rightOffset * 2) {
-    const newWidth = screen.width - rightOffset * 2;
-    hamburgerMenuEl.style.width = newWidth + "px";
-  } else if (screen.width > width + rightOffset * 2) {
-    hamburgerMenuEl.style.width = "35rem";
-  }
+  menuEl.classList.toggle("show");
+  // If hiding the menu
+  if (!menuEl.classList.contains("show")) return;
+  // If showing the menu
+  resizeMenu();
 });
 
 window.addEventListener("click", (e) => {
-  hamburgerMenuEl.classList.add("hide");
+  menuEl.classList.remove("show");
 });
 
 window.addEventListener("resize", (e) => {
-  if (screen.width >= 768) {
-    hamburgerMenuEl.classList.add("hide");
+  menuEl.classList.remove("show");
+  if (window.innerWidth >= 768) {
+    menuEl.style.width = "auto";
   }
 });
+
+function resizeMenu() {
+  const { right, width } = menuEl.getBoundingClientRect();
+  const rightOffset = window.innerWidth - right;
+  // If the window width is less than the menu element,
+  // resize the menu element
+  // Otherwise, if the window width is greater than the menu element,
+  // reset the menu element to its default width
+  if (window.innerWidth <= menuWidth + rightOffset * 2) {
+    const newWidth = window.innerWidth - rightOffset * 2;
+    menuEl.style.width = newWidth + "px";
+  } else {
+    menuEl.style.width = menuWidth + "px";
+  }
+}
